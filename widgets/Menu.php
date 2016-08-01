a33a96bff421a38fefb2f618a512a569cf1f02cd
suryana-itconcept <suryana@itconcept.sg>
2016-08-01 14:55:10 +0700

    override Menu, because active class must in <a> tag not in <li> tag

---
+++ widgets/Menu.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace sheillendra\proui\widgets;
+
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+use yii\helpers\Url;
+
+class Menu extends \yii\widgets\Menu {
+
+    public $linkTemplate = '<a href="{url}" {active}>{label}</a>';
+
+    protected function renderItems($items) {
+        $n = count($items);
+        $lines = [];
+        foreach ($items as $i => $item) {
+            $options = array_merge($this->itemOptions, ArrayHelper::getValue($item, 'options', []));
+            $tag = ArrayHelper::remove($options, 'tag', 'li');
+            $class = [];
+//            if ($item['active']) {
+//                $class[] = $this->activeCssClass;
+//            }
+            if ($i === 0 && $this->firstItemCssClass !== null) {
+                $class[] = $this->firstItemCssClass;
+            }
+            if ($i === $n - 1 && $this->lastItemCssClass !== null) {
+                $class[] = $this->lastItemCssClass;
+            }
+            if (!empty($class)) {
+                if (empty($options['class'])) {
+                    $options['class'] = implode(' ', $class);
+                } else {
+                    $options['class'] .= ' ' . implode(' ', $class);
+                }
+            }
+
+            $menu = $this->renderItem($item);
+            if (!empty($item['items'])) {
+                $submenuTemplate = ArrayHelper::getValue($item, 'submenuTemplate', $this->submenuTemplate);
+                $menu .= strtr($submenuTemplate, [
+                    '{items}' => $this->renderItems($item['items']),
+                ]);
+            }
+            $lines[] = Html::tag($tag, $menu, $options);
+        }
+
+        return implode("\n", $lines);
+    }
+
+    protected function renderItem($item) {
+        if (isset($item['url'])) {
+            $template = ArrayHelper::getValue($item, 'template', $this->linkTemplate);
+
+            return strtr($template, [
+                '{url}' => Html::encode(Url::to($item['url'])),
+                '{label}' => $item['label'],
+                '{active}' => $item['active'] ? 'class="active"' : ''
+            ]);
+        } else {
+            $template = ArrayHelper::getValue($item, 'template', $this->labelTemplate);
+
+            return strtr($template, [
+                '{label}' => $item['label'],
+            ]);
+        }
+    }
+
+}
