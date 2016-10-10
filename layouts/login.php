1bdc0d7b9e4dbb8193e5fbbcb2fd9b8bdbdab5c4
suryana-itconcept <suryana@itconcept.sg>
2016-10-10 08:59:28 +0700

    init

--- layouts/login.php
+++ layouts/login.php
@@ -1,4 +1,6 @@
 <?php
+
+use yii\helpers\Html;
 use sheillendra\proui\assets\ProuiAsset;
 
 $prouiAsset = ProuiAsset::register($this);
@@ -19,32 +21,33 @@ $prouiAsset = ProuiAsset::register($this);
 
         <!-- Icons -->
         <!-- The following icons can be replaced with your own, they are used by desktop and mobile browsers -->
-        <link rel="shortcut icon" href="<?php echo $prouiAsset->baseUrl;?>/img/favicon.png">
-        <link rel="apple-touch-icon" href="<?php echo $prouiAsset->baseUrl;?>/img/icon57.png" sizes="57x57">
-        <link rel="apple-touch-icon" href="<?php echo $prouiAsset->baseUrl;?>/img/icon72.png" sizes="72x72">
-        <link rel="apple-touch-icon" href="<?php echo $prouiAsset->baseUrl;?>/img/icon76.png" sizes="76x76">
-        <link rel="apple-touch-icon" href="<?php echo $prouiAsset->baseUrl;?>/img/icon114.png" sizes="114x114">
-        <link rel="apple-touch-icon" href="<?php echo $prouiAsset->baseUrl;?>/img/icon120.png" sizes="120x120">
-        <link rel="apple-touch-icon" href="<?php echo $prouiAsset->baseUrl;?>/img/icon144.png" sizes="144x144">
-        <link rel="apple-touch-icon" href="<?php echo $prouiAsset->baseUrl;?>/img/icon152.png" sizes="152x152">
-        <link rel="apple-touch-icon" href="<?php echo $prouiAsset->baseUrl;?>/img/icon180.png" sizes="180x180">
+        <link rel="shortcut icon" href="<?php echo $prouiAsset->baseUrl; ?>/img/favicon.png">
+        <link rel="apple-touch-icon" href="<?php echo $prouiAsset->baseUrl; ?>/img/icon57.png" sizes="57x57">
+        <link rel="apple-touch-icon" href="<?php echo $prouiAsset->baseUrl; ?>/img/icon72.png" sizes="72x72">
+        <link rel="apple-touch-icon" href="<?php echo $prouiAsset->baseUrl; ?>/img/icon76.png" sizes="76x76">
+        <link rel="apple-touch-icon" href="<?php echo $prouiAsset->baseUrl; ?>/img/icon114.png" sizes="114x114">
+        <link rel="apple-touch-icon" href="<?php echo $prouiAsset->baseUrl; ?>/img/icon120.png" sizes="120x120">
+        <link rel="apple-touch-icon" href="<?php echo $prouiAsset->baseUrl; ?>/img/icon144.png" sizes="144x144">
+        <link rel="apple-touch-icon" href="<?php echo $prouiAsset->baseUrl; ?>/img/icon152.png" sizes="152x152">
+        <link rel="apple-touch-icon" href="<?php echo $prouiAsset->baseUrl; ?>/img/icon180.png" sizes="180x180">
         <!-- END Icons -->
         <?= Html::csrfMetaTags() ?>
         <title><?php echo Html::encode($this->title) ?></title>
-        <?php $this->head() ?>
+<?php $this->head() ?>
     </head>
     <body>
-        <?php $this->beginBody() ?>
+<?php $this->beginBody() ?>
         <!-- Login Background -->
         <div id="login-background">
             <!-- For best results use an image with a resolution of 2560x400 pixels (prefer a blurred image for smaller file size) -->
-            <img src="<?php echo $prouiAsset->baseUrl;?>/img/placeholders/headers/login_header.jpg" alt="Login Background" class="animation-pulseSlow">
+            <img src="<?php echo $prouiAsset->baseUrl; ?>/img/placeholders/headers/login_header.jpg" alt="Login Background" class="animation-pulseSlow">
         </div>
         <!-- END Login Background -->
 
-        <?php echo $content;?>
+        <?php echo $content; ?>
 
-        <?php $this->endBody() ?>
+<?php $this->endBody() ?>
     </body>
 </html>
-<?php $this->endPage();
+<?php
+$this->endPage();
